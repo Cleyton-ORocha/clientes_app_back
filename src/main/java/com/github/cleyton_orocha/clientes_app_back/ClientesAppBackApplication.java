e5babe640ad3ccec2fd3506a88523e860895fc55
cleyton <cleytoodeoliveirarocha@gmail.com>
2024-04-14 10:05:58 -0300

    init: clientes_app_back

---
+++ src/main/java/com/github/cleyton_orocha/clientes_app_back/ClientesAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.github.cleyton_orocha.clientes_app_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesAppBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/cleyton_orocha/clientes_app_back/ClientesAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cleyton_orocha.clientes_app_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
