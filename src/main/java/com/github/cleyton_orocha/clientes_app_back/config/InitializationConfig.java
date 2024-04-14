eb4bbb28cfc48e9ddf9cd14ed4c0cf99235040e9
cleyton <cleytoodeoliveirarocha@gmail.com>
2024-04-14 14:46:47 -0300

    feat: messages api Init

---
+++ src/main/java/com/github/cleyton_orocha/clientes_app_back/config/InitializationConfig.java
@@ -0,0 +1,28 @@
+package com.github.cleyton_orocha.clientes_app_back.config;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class InitializationConfig {
+    @Bean
+    public MessageSource messageSource(){
+        var messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("ISO-8859-1");
+        messageSource.setDefaultLocale(Locale.getDefault());
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validatorFactoryBean(){
+        var bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+}
