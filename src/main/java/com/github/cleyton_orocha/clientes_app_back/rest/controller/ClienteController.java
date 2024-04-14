05b0501193f2031533c7434d6ee3e2ef0613eb9b
cleyton <cleytoodeoliveirarocha@gmail.com>
2024-04-14 14:04:45 -0300

    feat: cliente findById

--- src/main/java/com/github/cleyton_orocha/clientes_app_back/rest/controller/ClienteController.java
+++ src/main/java/com/github/cleyton_orocha/clientes_app_back/rest/controller/ClienteController.java
@@ -1,11 +1,14 @@
 package com.github.cleyton_orocha.clientes_app_back.rest.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.github.cleyton_orocha.clientes_app_back.model.entity.Cliente;
 import com.github.cleyton_orocha.clientes_app_back.model.repository.ClienteRepository;
@@ -16,12 +19,19 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @RequestMapping("api/clientes")
 public class ClienteController {
-    
+
     private final ClienteRepository clienteRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente save( @RequestBody Cliente cliente ){
+    public Cliente save(@RequestBody Cliente cliente) {
         return clienteRepository.save(cliente);
     }
+
+    @GetMapping("{id}")
+    public Cliente findById(@PathVariable Integer id) {
+        return clienteRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
 }
